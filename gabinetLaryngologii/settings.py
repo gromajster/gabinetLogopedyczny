94065c2fc871d17fc0fa77f1d71f84386d46044e
Michal Kucko <mkucko145@gmail.com>
2019-11-22 09:07:38 +0100

    migrations

--- gabinetLaryngologii/settings.py
+++ gabinetLaryngologii/settings.py
@@ -132,3 +132,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
+
+if 'DATABASE_URL' in os.environ:
+    import dj_database_url
+    DATABASES = {'default': dj_database_url.config()}
