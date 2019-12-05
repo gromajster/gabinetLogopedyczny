dbd00af5cbfb1ad94e4d3ab8e19a731501469171
Michal Kucko <mkucko145@gmail.com>
2019-12-05 21:56:56 +0100

    contact email done

--- gabinetLaryngologii/contact/views.py
+++ gabinetLaryngologii/contact/views.py
@@ -1,6 +1,8 @@
 from rest_framework import views
 from rest_framework.response import Response
+from django.core.mail import send_mail
 
+from gabinetLaryngologii import settings
 from .serializers import ContactSerializer
 
 
@@ -10,5 +12,11 @@ class ContactView(views.APIView):
         serializer = ContactSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         contact_form = serializer.data
-
+        message = f"Imię: {contact_form.get('name')} \n" \
+                  f"Nazwisko: {contact_form.get('surname')} \n" \
+                  f"E-mail: {contact_form.get('email')} \n" \
+                  f"Tel.: {contact_form.get('phone_number')} \n" \
+                  f"Treść wiadomości: \n" \
+                  f"{contact_form.get('message')}"
+        send_mail('Wiadomość Gabinet Laryngologiczny', message, settings.EMAIL_HOST_USER, ['mkucko145@gmail.com'])
         return Response({"message": "Wiadomość została poprawnie wysłana."}, status=200)
--- gabinetLaryngologii/settings.py
+++ gabinetLaryngologii/settings.py
@@ -16,7 +16,6 @@ import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -27,7 +26,6 @@ SECRET_KEY = 'q%u&ir4%0jw)l(tc$1#qq)0)w%7n)*9v$jj=$$*u@i99*x^(&f'
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-
 
 # Application definition
 
@@ -83,7 +81,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'gabinetLaryngologii.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
@@ -93,7 +90,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -113,7 +109,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
@@ -123,11 +118,9 @@ DATE_FORMAT = ['%d-%m-%Y']
 
 DATE_INPUT_FORMATS = ['%d-%m-%Y']
 
-TIME_ZONE = 'UTC'
-
+TIME_ZONE = 'Europe/Warsaw'
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
@@ -142,3 +135,10 @@ REST_FRAMEWORK = {
 
 if 'DATABASE_URL' in os.environ:
     DATABASES = {'default': dj_database_url.config()}
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'gabinet.laryngologiczny.gdynia@gmail.com'
+EMAIL_HOST_PASSWORD = '!@#$%qazWSX'
