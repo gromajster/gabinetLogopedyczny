ed324136fda72b7bedd25b1d37b2495e9ed9243b
Michal Kucko <mkucko145@gmail.com>
2019-12-08 11:15:41 +0100

    Post model file field changed to optional

--- gabinetLaryngologii/blog/models.py
+++ gabinetLaryngologii/blog/models.py
@@ -7,6 +7,6 @@ class Post(models.Model):
     title = models.CharField(max_length=50)
     published_date = models.DateField(auto_now=False)
     published_time = models.TimeField(auto_now=False)
-    picture_url = models.FileField(upload_to='blog_pictures/')
+    picture_url = models.FileField(upload_to='blog_pictures/', blank=True, null=True)
     long_text = models.TextField(max_length=1000)
     published = models.BooleanField(default=False)
