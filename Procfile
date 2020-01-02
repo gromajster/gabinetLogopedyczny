890637a26ef5642bdf86cd1e660cff32918f5865
Michal Kucko <mkucko145@gmail.com>
2020-01-02 18:13:02 +0100

    Dynos for celery

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: gunicorn gabinetLaryngologii.wsgi --log-file -
+worker: celery -A gabinetLaryngologii worker -l info
--- gabinetLaryngologii/contact/celery_tasks.py
+++ gabinetLaryngologii/contact/celery_tasks.py
@@ -4,6 +4,7 @@ from django.core.mail import send_mail
 from gabinetLaryngologii import settings
 
 
+
 @shared_task
 def send_email_task(contact_form):
     message = f"Imię: {contact_form.get('name')} \n" \
