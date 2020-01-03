5457981f0a69873d3f0f5761ffab57ad811c5e1a
Michal Kucko <mkucko145@gmail.com>
2020-01-03 12:56:42 +0100

    Added better admin view for Media model

--- gabinetLaryngologii/material/admin.py
+++ gabinetLaryngologii/material/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Media
 
-admin.site.register(Media)
+
+@admin.register(Media)
+class MediaAdmin(admin.ModelAdmin):
+    list_display = ('media_name', 'media_description', 'extension')
+    search_fields = ('media_name',)
+    ordering = ('id',)
