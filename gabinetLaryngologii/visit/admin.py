5ffd0d47601a1d7629469b08c8ce5203a37b6fca
Michal Kucko <mkucko145@gmail.com>
2020-01-06 20:27:13 +0100

    Fix models fro appointments

--- gabinetLaryngologii/visit/admin.py
+++ gabinetLaryngologii/visit/admin.py
@@ -8,14 +8,14 @@ from gabinetLaryngologii.visit.models import Appointment
 class AppointmentForm(ModelForm):
     class Meta:
         model = Appointment
-        exclude = ['appointment_status', 'email', 'name', 'surname']
+        exclude = ['appointment_status', 'email', 'surname']
 
 
 class AppointmentAdmin(ModelAdmin):
     form = AppointmentForm
-    list_display = ('name', 'surname', 'email', 'appointment_date', 'appointment_time', 'appointment_status')
+    list_display = ('surname', 'email', 'appointment_date', 'appointment_time', 'appointment_status')
     list_filter = ('appointment_status', 'appointment_time', 'appointment_date')
-    search_fields = ('surname', 'name', 'email')
+    search_fields = ('surname', 'email')
     date_hierarchy = 'appointment_date'
     ordering = ('appointment_date',)
 
