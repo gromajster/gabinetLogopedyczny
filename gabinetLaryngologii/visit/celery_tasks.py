a2ee7db09c5280b506a1ae7f271b366d8b7b5b92
Michal Kucko <mkucko145@gmail.com>
2020-01-08 12:08:36 +0100

    Celery task to send appointment confirmation email

--- gabinetLaryngologii/visit/celery_tasks.py
+++ gabinetLaryngologii/visit/celery_tasks.py
@@ -1,24 +1,28 @@
 from celery import shared_task
-from django.core.mail import send_mail
+from django.core.mail import send_mail, EmailMessage
 from django.template.loader import get_template
 from django.utils.html import strip_tags
+
+from gabinetLaryngologii import settings
 
 
 @shared_task
 def send_confirmation_email(email, subscription_confirmation_url):
-    data = dict()
-    data["confirmation_url"] = subscription_confirmation_url
-    data["subject"] = "Please confirm the subscription"
-    data["email"] = email
-    data["project_name"] = "Scientific Dev"
-    data["site_url"] = "https://www.scientificdev.net"
-    data["contact_us_url"] = "https://www.scientificdev.net/contact/"
-    template = get_template("visit_confirmation/confirmation_mail.html")
-    data["html_text"] = template.render(data)
-    data["plain_text"] = strip_tags(data["html_text"])
-
-    send_mail(data["subject"], data["plain_text"], 'gabinetLogopedzcynz@gmail.com', [data['email']])
+    # data = dict()
+    # data["confirmation_url"] = subscription_confirmation_url
+    # data["subject"] = "Please confirm the subscription"
+    # data["email"] = email
+    # data["project_name"] = "Scientific Dev"
+    # data["site_url"] = "https://www.scientificdev.net"
+    # data["contact_us_url"] = "https://www.scientificdev.net/contact/"
+    # template = get_template("visit_confirmation/confirmation_mail.html")
+    # data["html_text"] = template.render(data)
+    # data["plain_text"] = strip_tags(data["html_text"])
+    #
+    # # send_mail(data["subject"], data["plain_text"], settings.EMAIL_HOST_USER, [data['email']])
     # email_message = EmailMessage(data["subject"], data["plain_text"], 'artur@scientificdev.net',
     #                              [data['email']])
     # email_message.send(fail_silently=False)
+    message = subscription_confirmation_url
+    send_mail('Wiadomość Gabinet Logopedyczny', message, settings.EMAIL_HOST_USER, [email])
     return None
