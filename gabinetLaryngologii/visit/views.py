120a9d741d3d3eabbaca833c7e6eeffc642c24eb
Michal Kucko <mkucko145@gmail.com>
2020-01-08 20:40:07 +0100

    Model Appointment querryset updated

--- gabinetLaryngologii/visit/views.py
+++ gabinetLaryngologii/visit/views.py
@@ -1,16 +1,15 @@
-from rest_framework.generics import GenericAPIView
-from rest_framework.mixins import UpdateModelMixin
-from rest_framework.response import Response
-from rest_framework import viewsets, views, status
 
-from gabinetLaryngologii.visit.celery_tasks import send_confirmation_email
+from rest_framework.response import Response
+from rest_framework import viewsets
+
+
 from gabinetLaryngologii.visit.models import Appointment
 from gabinetLaryngologii.visit.serializers import AppointmentSerializer, AppointmentUpdateSerializer
 from gabinetLaryngologii.visit.token_handler import token_generator, encrypt
 
 
 class AppointmentViewSet(viewsets.ModelViewSet):
-    queryset = Appointment.objects.all()
+    queryset = Appointment.objects.all().filter(appointment_status="open")
     serializer_class = AppointmentSerializer
     pagination_class = None
     http_method_names = ['get', 'patch']
